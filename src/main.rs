e03199fc14463741478076698a73f3d28296d640
MisakaYuma <okyu122515@outlook.jp>
2026-05-15 23:34:27 +0900

    サーバの起動

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+// anyhowはまだ使ってないから警告出るけど消さないで
+use anyhow::Result;
+use axum::Router;
+use tokio;
+
+#[tokio::main]
+async fn main() {
+    // ルータの作成
+    // エンドポイントと，ハンドラと呼ばれるリクエストに対してどんなレスポンスを返すかの関数も後に実装
+    let app = Router::new();
+
+    // TCPリスナの作成
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+
+    println!("listening on {:?}", listener.local_addr().unwrap());
+
+    // HTTPサーバの起動
+    axum::serve(listener, app).await.unwrap();
 }
